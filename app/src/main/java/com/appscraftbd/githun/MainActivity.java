6aad8f036734780a6dba28577fb05457d0238fd6
appscraft.bd <appscraftbd@gmail.com>
2023-10-01 10:34:25 +0600

    Initial commit

---
+++ app/src/main/java/com/appscraftbd/githun/MainActivity.java
@@ -0,0 +1,20 @@
+package com.appscraftbd.githun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appscraftbd.localjson.Localjson;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Localjson localjson;
+
+
+    }
+}
